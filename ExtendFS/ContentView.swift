33371400e902dedbeb50101078aec52d802e6c22
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2025-11-19 19:05:49 -0500

    Show extension enablement state in UI

--- ExtendFS/ContentView.swift
+++ ExtendFS/ContentView.swift
@@ -7,27 +7,83 @@
 
 import SwiftUI
 import FSKit
+import ServiceManagement
+
+enum ExtensionActivationState {
+    case inactive
+    case active
+    case notDetermined
+}
 
 struct ContentView: View {
-    @State private var modules: [FSModuleIdentity] = []
+    nonisolated static let logger = Logger(subsystem: "com.kpchew.ExtendFS", category: "default")
+    
+    let ext4ExtensionIdentifier = "com.kpchew.ExtendFS.ext4Extension"
+    @State private var ext4ExtensionState: ExtensionActivationState = .notDetermined
+    
+    let osVersion = ProcessInfo.processInfo.operatingSystemVersion
+    
+    let timer = Timer.publish(every: 5.0, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack {
-            ForEach(modules) {
-                Text($0.description)
-                Text($0.bundleIdentifier)
-            }
-            Button("Reload") {
-                Logger().log("fetching extensions")
-                FSClient.shared.fetchInstalledExtensions { modules, err in
-                    Logger().log("fetched extensions")
-                    if let modules {
-                        self.modules = modules
-                    }
+            switch ext4ExtensionState {
+            case .inactive:
+                Text("Extension needs to be enabled in System Settings")
+                Button("Open System Settings") {
+                    SMAppService.openSystemSettingsLoginItems()
+                }
+            case .active:
+                Text("Extension is active and ready")
+            case .notDetermined:
+                if osVersion.majorVersion < 26 {
+                    Text("The app cannot tell if the extension is enabled on versions of macOS before macOS Tahoe 26.0. However, it will still function if enabled.")
+                } else {
+                    Text("An error occurred while determining whether the extension is enabled.")
+                }
+                Button("Open System Settings") {
+                    SMAppService.openSystemSettingsLoginItems()
                 }
             }
         }
         .padding()
+        .onReceive(timer) { _ in
+            Task {
+                await updateExtensionEnablementState()
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: NSApplication.willBecomeActiveNotification), perform: { output in
+            Task {
+                await updateExtensionEnablementState()
+            }
+        })
+        .task {
+            await updateExtensionEnablementState()
+        }
+    }
+    
+    func updateExtensionEnablementState() async {
+        let states = await checkExtensionEnablementState()
+        
+        self.ext4ExtensionState = states[ext4ExtensionIdentifier] ?? .notDetermined
+    }
+    
+    nonisolated func checkExtensionEnablementState() async -> [String: ExtensionActivationState] {
+        var states = [
+            ext4ExtensionIdentifier: ExtensionActivationState.notDetermined
+        ]
+        do {
+            let extensions = try await FSClient.shared.installedExtensions
+            
+            let ext4Ext = extensions.first { $0.bundleIdentifier == ext4ExtensionIdentifier }
+            if let ext4Ext {
+                states[ext4ExtensionIdentifier] = ext4Ext.isEnabled ? .active : .inactive
+            }
+        } catch {
+            Self.logger.error("Failed to fetch installed FSKit extensions: \(error)")
+        }
+        
+        return states
     }
 }
 
--- ExtendFS/ExtendFSApp.swift
+++ ExtendFS/ExtendFSApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct ExtendFSApp: App {
     var body: some Scene {
-        WindowGroup {
+        Window("ExtendFS", id: "main") {
             ContentView()
         }
     }
